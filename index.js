a106921ef46b123861a9b526d4b44ca0a64be626
jojoeny <jojoeny@gmail.com>
2022-01-31 12:56:46 +0100

    git committed

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const express = require('express');
 const memeRoute = require('./routes/memes');
 const mongoose = require('mongoose');
 const bodyParser = require("body-parser");
-const Jimp = require("jimp")
-
+const Jimp = require("jimp");
+require('dotenv').config();
 
 const port = process.env.PORT || 5000;
 const app = express();
-mongoose.connect("mongodb+srv://joha:oymoma@cluster0.qfjk5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");          
+const connection_uri = process.env.CONNECTION_URI;
+mongoose.connect(connection_uri);          
 
 app.use('/uploads', express.static('uploads'));
 app.use(bodyParser.urlencoded({ extended: false }));
